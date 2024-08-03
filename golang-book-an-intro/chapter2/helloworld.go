c99017e8c5febf9aab0f62ba1e84093c7bd2e1b5
Ariel Gugliotta <arielgugliotta89@gmail.com>
2024-08-02 20:51:56 -0700

    Excercise 2 - Add new Println

--- golang-book-an-intro/chapter2/helloworld.go
+++ golang-book-an-intro/chapter2/helloworld.go
@@ -1,6 +1,9 @@
 package main
-import "fmt" 
+
+import "fmt"
+
 // this is a comment
 func main() {
-    fmt.Println("Hello World")
+	fmt.Println("Hello World")
+	fmt.Println("Hello, my name is Ariel")
 }
