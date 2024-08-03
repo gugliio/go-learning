639944f595c98c1752923763cd51c2ca669bfb21
Ariel Gugliotta <arielgugliotta89@gmail.com>
2024-08-02 21:16:42 -0700

    Go an introduction - Chapter 3

---
+++ golang-book-an-intro/chapter3/helloworld.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("----------------------------------")
+	fmt.Println("Strings")
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World")
+
+	fmt.Println("----------------------------------")
+	fmt.Println("Boolean")
+	fmt.Println(true && true)
+    fmt.Println(true && false)
+    fmt.Println(true || true)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+	fmt.Println("----------------------------------")
+}
